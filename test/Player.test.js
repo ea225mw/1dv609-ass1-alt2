503d149688dee12b753c2b585846ac9369adc19a
Emanuel Andersen <ea225mw@student.lnu.se>
2025-11-24 14:03:25 +0100

    RED: ReferenceError: Player is not defined.

---
+++ test/Player.test.js
@@ -0,0 +1,13 @@
+/*
+1. Return players name
+2. Amount of funds
+3. Bets?
+4. Constructor
+  - Create funds
+  - Set name
+
+*/
+
+test("should return player's name", () => {
+  const player = new Player()
+})
