e5e84e45307825d70f5d34105003bbc8e71c5d47
Emanuel Andersen <ea225mw@student.lnu.se>
2025-11-24 17:53:31 +0100

    🔴 RED: TypeError: menuHandler.handleMenuChoice is not a function.

--- test/MenuHandler.test.js
+++ test/MenuHandler.test.js
@@ -2,4 +2,5 @@ import {MenuHandler} from '../src/MenuHandler.js'
 
 test('should return startGame on input 1', () => {
   const menuHandler = new MenuHandler()
+  expect(menuHandler.handleMenuChoice('1')).toBe('startGame')
 })
