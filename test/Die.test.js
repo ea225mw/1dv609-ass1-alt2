a419dd0fd3888ef8864118f372fb323aa0cc857c
Emanuel Andersen <ea225mw@student.lnu.se>
2025-11-24 15:36:28 +0100

    🔴 RED: In Die: die.getFaceValue is not a function.

--- test/Die.test.js
+++ test/Die.test.js
@@ -3,11 +3,13 @@ import {Die} from '../src/Die.js'
 const die = new Die()
 
 test('should return a number equal to or greater than 1', () => {
-  const value = die.roll()
+  die.roll()
+  const value = die.getFaceValue()
   expect(value).toBeGreaterThanOrEqual(1)
 })
 
 test('should return a number equal to or less than 6', () => {
-  const value = die.roll()
+  die.roll()
+  const value = die.getFaceValue()
   expect(value).toBeLessThanOrEqual(6)
 })
