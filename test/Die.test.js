5d244ce55be32d863fc1ea8511bd9ae75fbbaf06
Emanuel Andersen <ea225mw@student.lnu.se>
2025-11-24 14:00:43 +0100

    REFACTOR: Remove duplicated die object creation.

--- test/Die.test.js
+++ test/Die.test.js
@@ -1,13 +1,13 @@
 import {Die} from '../src/Die.js'
 
+const die = new Die()
+
 test('should return a number equal to or greater than 1', () => {
-  const die = new Die()
   const value = die.roll()
   expect(value).toBeGreaterThanOrEqual(1)
 })
 
 test('should return a number equal to or less than 6', () => {
-  const die = new Die()
   const value = die.roll()
   expect(value).toBeLessThanOrEqual(6)
 })
