98ce0936d09e3dda850fe444e99e3a963f807584
Emanuel Andersen <ea225mw@student.lnu.se>
2025-11-24 14:49:50 +0100

    🟢 GREEN: Implement getFunds().

--- src/Player.js
+++ src/Player.js
@@ -1,11 +1,17 @@
 export class Player {
   name
+  #funds
 
   constructor(name) {
     this.name = name
+    this.#funds = 500
   }
 
   getName() {
     return this.name
+  }
+
+  getFunds() {
+    return this.#funds
   }
 }
