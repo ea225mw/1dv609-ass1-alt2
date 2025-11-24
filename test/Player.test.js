a78c2e8b2cb1cb220bcacf7acdcede3b2da9c6e3
Emanuel Andersen <ea225mw@student.lnu.se>
2025-11-24 14:47:37 +0100

    🔴 RED: player.getFunds is not a function.

--- test/Player.test.js
+++ test/Player.test.js
@@ -15,3 +15,8 @@ test("should return player's name", () => {
   const name = player.getName()
   expect(name).toBe('Joe')
 })
+
+test("should return a player's initial total funds on creation", () => {
+  const player = new Player('Joe')
+  expect(player.getFunds()).toBe(500)
+})
