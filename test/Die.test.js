a3e250043e5acd1bb57483d45f559099da18dd3d
Emanuel Andersen <ea225mw@student.lnu.se>
2025-11-24 13:45:13 +0100

    GREEN BEFORE RED: The implementation was made so that test #2 passed before going to RED.

--- test/Die.test.js
+++ test/Die.test.js
@@ -5,3 +5,9 @@ test('should return a number equal to or greater than 1', () => {
   const value = die.roll()
   expect(value).toBeGreaterThanOrEqual(1)
 })
+
+test('should return a number equal to or less than 6', () => {
+  const die = new Die()
+  const value = die.roll()
+  expect(value).toBeLessThanOrEqual(6)
+})
