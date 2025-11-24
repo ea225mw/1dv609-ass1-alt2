c33f0564772f1cdfdeeca493557a795d59b6e6b4
Emanuel Andersen <ea225mw@student.lnu.se>
2025-11-24 12:06:23 +0100

    REFACTOR: Remove comments.

--- test/Die.test.js
+++ test/Die.test.js
@@ -3,6 +3,3 @@ import {Die} from '../src/Die.js'
 test('should return a number equal to or greater than 1', () => {
   const die = new Die()
 })
-
-/*const value = die.roll()
-  expect(value).toBeGreaterThanOrEqualTo(1)*/
