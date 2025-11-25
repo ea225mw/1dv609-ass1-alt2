7e95b8360d6ae172c8487c829775b0af067ad0fa
Emanuel Andersen <ea225mw@student.lnu.se>
2025-11-25 07:56:10 +0100

    🔴 RED: expected guitGame, got undefined.

--- test/MenuHandler.test.js
+++ test/MenuHandler.test.js
@@ -1,6 +1,11 @@
 import {MenuHandler} from '../src/MenuHandler.js'
 
-test('should return startGame on input 1', () => {
+test('should return "startGame" on input 1', () => {
   const menuHandler = new MenuHandler()
   expect(menuHandler.handleMenuChoice('1')).toBe('startGame')
+})
+
+test('should return "quitGame" on input 9', () => {
+  const menuHandler = new MenuHandler()
+  expect(menuHandler.handleMenuChoice('9')).toBe('quitGame')
 })
