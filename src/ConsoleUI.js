3b43dca1152adea44e72d4da0e18cd84e90a4644
Emanuel Andersen <ea225mw@student.lnu.se>
2025-11-25 17:00:25 +0100

    🟡 REFACTOR: Refactor ConsolUI.

---
+++ src/ConsoleUI.js
@@ -0,0 +1,38 @@
+export class ConsoleUI {
+  #rl
+
+  constructor(rl) {
+    this.#rl = rl
+  }
+
+  printGameBanner() {
+    console.log('\n=== Bankirspelet ===')
+  }
+
+  async printStartMenu() {
+    console.log('1) Start game')
+    console.log('9) Exit')
+
+    const answer = await this.#rl.question(
+      'Choose an alternative from the menu. Type the corresponding number and press Enter. '
+    )
+    return answer
+  }
+
+  async promptForNumberOfPlayers() {
+    const numberOfPlayers = await this.#rl.question('How many players? ')
+    return numberOfPlayers
+  }
+
+  async promptForPlayerNames(numberOfPlayers) {
+    let arrayWithNames = []
+    let index = 1
+    while (index <= numberOfPlayers) {
+      const name = await this.#rl.question(`Enter the name of player ${index}: `)
+      console.log(name)
+      arrayWithNames.push(name)
+      index++
+    }
+    return arrayWithNames
+  }
+}
