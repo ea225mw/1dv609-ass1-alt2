5744753228f674abd3f1a4dc7983f2b67e5bbae8
Emanuel Andersen <ea225mw@student.lnu.se>
2025-11-25 07:59:22 +0100

    🟡 REFACTOR: Remove duplication in MenuHandler.test.js.

--- test/MenuHandler.test.js
+++ test/MenuHandler.test.js
@@ -1,11 +1,11 @@
 import {MenuHandler} from '../src/MenuHandler.js'
 
+const menuHandler = new MenuHandler()
+
 test('should return "startGame" on input 1', () => {
-  const menuHandler = new MenuHandler()
   expect(menuHandler.handleMenuChoice('1')).toBe('startGame')
 })
 
 test('should return "quitGame" on input 9', () => {
-  const menuHandler = new MenuHandler()
   expect(menuHandler.handleMenuChoice('9')).toBe('quitGame')
 })
