41a29c988329a59404748420bbae7ca5d12bae77
Emanuel Andersen <ea225mw@student.lnu.se>
2025-11-25 11:10:52 +0100

    🟡 REFACTOR: Remove duplication in GameController.test.js.

--- test/GameController.test.js
+++ test/GameController.test.js
@@ -1,15 +1,15 @@
 import {jest} from '@jest/globals'
 import {GameController} from '../src/GameController.js'
-import {Player} from '../src/Player.js'
+/*import {Player} from '../src/Player.js'
 import {ConsoleUI} from '../temp/ConsoleUI.js'
-import {MenuHandler} from '../src/MenuHandler.js'
+import {MenuHandler} from '../src/MenuHandler.js'*/
+
 /* 
 1. Visa startmenyn
 2. Välja starta spelet
 3. Ange antal spelare
 4. Skriva in alla spelares namn och skapa upp spelarobjekt med tillhörande tärningar.
 5. Spelarna ska läggas till i arrayen PLAYERS
-
 */
 
 const consoleUIMock = {
@@ -17,14 +17,14 @@ const consoleUIMock = {
   printStartMenu: jest.fn().mockResolvedValue('1'),
 }
 
+const gameController = new GameController(consoleUIMock)
+
 test('should print game name banner on startup', () => {
-  const gameController = new GameController(consoleUIMock)
   gameController.run()
   expect(consoleUIMock.printGameBanner).toHaveBeenCalled()
 })
 
 test('should print start menu on startup', () => {
-  const gameController = new GameController(consoleUIMock)
   gameController.run()
   expect(consoleUIMock.printStartMenu).toHaveBeenCalled()
 })
