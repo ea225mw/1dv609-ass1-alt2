6a597539a200e45fb7d9ccaae1cd8b2abf508c1a
Emanuel Andersen <ea225mw@student.lnu.se>
2025-11-25 18:50:48 +0100

    🔴 RED: Cannot find module '..src/PlayerFactory.js'

---
+++ test/PlayerFactory.test.js
@@ -0,0 +1,8 @@
+import {PlayerFactory} from '..src/PlayerFactory.js'
+
+test('should return a player object', () => {
+  const factory = new PlayerFactory()
+  const player = factory.create('John')
+
+  expect(player.getName()).toBe('John')
+})
