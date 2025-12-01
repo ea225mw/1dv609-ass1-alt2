7d35740b223a61318510af715ff1c64ac34d9045
Emanuel Andersen <ea225mw@student.lnu.se>
2025-12-01 15:02:39 +0100

    🔴 RED: TypeError: banker.rollDie is not a function

--- test/Banker.test.js
+++ test/Banker.test.js
@@ -1,9 +1,17 @@
+import {jest, test} from '@jest/globals'
 import {Banker} from '../src/Banker.js'
 import {Die} from '../src/Die.js'
 
-const bankerDie = new Die()
-
 test('should create a new Banker object with a name property', () => {
+  const bankerDie = new Die()
   const banker = new Banker(bankerDie)
   expect(banker.getName()).toBe('Banker')
+})
+
+test('should return die face value', () => {
+  const dieMock = {
+    roll: jest.fn().mockReturnValue(3),
+  }
+  const banker = new Banker(dieMock)
+  expect(banker.rollDie()).toBe(3)
 })
